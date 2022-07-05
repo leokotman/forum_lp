e57081931bf995f69b7636fdb9137ec35de0b23e
Leo <leoktmn@yandex.ru>
2022-07-05 02:33:00 +0200

    added fonts

--- src/features/components/Logo.js
+++ src/features/components/Logo.js
@@ -1,12 +1,17 @@
+import "./Logo.scss";
+import logo from "../../assets/images/icons/logo.png";
+
 function Logo() {
-    return (
-        <div>
-            <img alt="main logo"/>
-            <div>
-                <h2>Security</h2>
-                <h5>Forum</h5>
-            </div>
-        </div>
-    );
-};
+	return (
+		<div>
+			<a href="/">
+				<img src={logo} alt="main logo" />
+			</a>
+			<div>
+				<h2>Security</h2>
+				<h5>Forum</h5>
+			</div>
+		</div>
+	);
+}
 export default Logo;
